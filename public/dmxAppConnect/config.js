5ad088285fa3aa098c20ea2b7b3e76ef3893e927
root <karlpallan@gmail.com>
2023-04-04 16:23:19 +0000

    first commit of express prod v2 site at UMG

---
+++ public/dmxAppConnect/config.js
@@ -0,0 +1,362 @@
+dmx.config({
+  "main": {
+    "query": [
+      {
+        "type": "text",
+        "name": "offset"
+      }
+    ]
+  },
+  "paging_test": {
+    "query": [
+      {
+        "type": "text",
+        "name": "offset"
+      }
+    ]
+  },
+  "gallery": {
+    "detail_Gallery": {
+      "meta": [
+        {
+          "name": "id_record",
+          "type": "text"
+        },
+        {
+          "name": "obj_status",
+          "type": "text"
+        },
+        {
+          "name": "obj_designation",
+          "type": "text"
+        },
+        {
+          "name": "obj_name",
+          "type": "text"
+        },
+        {
+          "name": "thumb",
+          "type": "text"
+        },
+        {
+          "name": "obj_label",
+          "type": "text"
+        },
+        {
+          "name": "caption",
+          "type": "text"
+        },
+        {
+          "name": "lote_number",
+          "type": "text"
+        },
+        {
+          "name": "find_location",
+          "type": "text"
+        },
+        {
+          "name": "date_documented",
+          "type": "text"
+        },
+        {
+          "name": "obj_description",
+          "type": "text"
+        },
+        {
+          "name": "set_timestamp",
+          "type": "text"
+        },
+        {
+          "name": "informe_designation",
+          "type": "text"
+        }
+      ],
+      "outputType": "array"
+    },
+    "data_detail1": {
+      "meta": [
+        {
+          "name": "id_record",
+          "type": "text"
+        },
+        {
+          "name": "obj_status",
+          "type": "text"
+        },
+        {
+          "name": "obj_designation",
+          "type": "text"
+        },
+        {
+          "name": "obj_name",
+          "type": "text"
+        },
+        {
+          "name": "thumb",
+          "type": "text"
+        },
+        {
+          "name": "obj_label",
+          "type": "text"
+        },
+        {
+          "name": "caption",
+          "type": "text"
+        },
+        {
+          "name": "lote_number",
+          "type": "text"
+        },
+        {
+          "name": "find_location",
+          "type": "text"
+        },
+        {
+          "name": "date_documented",
+          "type": "text"
+        },
+        {
+          "name": "obj_description",
+          "type": "text"
+        },
+        {
+          "name": "set_timestamp",
+          "type": "text"
+        },
+        {
+          "name": "informe_designation",
+          "type": "text"
+        }
+      ],
+      "outputType": "array"
+    },
+    "data_detail2": {
+      "meta": [
+        {
+          "name": "id_record",
+          "type": "text"
+        },
+        {
+          "name": "obj_status",
+          "type": "text"
+        },
+        {
+          "name": "obj_designation",
+          "type": "text"
+        },
+        {
+          "name": "obj_name",
+          "type": "text"
+        },
+        {
+          "name": "thumb",
+          "type": "text"
+        },
+        {
+          "name": "obj_label",
+          "type": "text"
+        },
+        {
+          "name": "caption",
+          "type": "text"
+        },
+        {
+          "name": "lote_number",
+          "type": "text"
+        },
+        {
+          "name": "find_location",
+          "type": "text"
+        },
+        {
+          "name": "date_documented",
+          "type": "text"
+        },
+        {
+          "name": "obj_description",
+          "type": "text"
+        },
+        {
+          "name": "set_timestamp",
+          "type": "text"
+        },
+        {
+          "name": "informe_designation",
+          "type": "text"
+        }
+      ],
+      "outputType": "array"
+    },
+    "ddGallery": {
+      "meta": [
+        {
+          "name": "id_record",
+          "type": "number"
+        },
+        {
+          "name": "obj_status",
+          "type": "text"
+        },
+        {
+          "name": "obj_designation",
+          "type": "text"
+        },
+        {
+          "name": "obj_name",
+          "type": "text"
+        },
+        {
+          "name": "obj_label",
+          "type": "text"
+        },
+        {
+          "name": "lote_number",
+          "type": "text"
+        },
+        {
+          "name": "find_location",
+          "type": "text"
+        },
+        {
+          "name": "date_documented",
+          "type": "text"
+        },
+        {
+          "name": "obj_description",
+          "type": "text"
+        },
+        {
+          "name": "set_timestamp",
+          "type": "text"
+        },
+        {
+          "name": "informe_designation",
+          "type": "text"
+        }
+      ],
+      "outputType": "array"
+    },
+    "deDetail2": {
+      "meta": [
+        {
+          "name": "id_record",
+          "type": "text"
+        },
+        {
+          "name": "obj_status",
+          "type": "text"
+        },
+        {
+          "name": "obj_designation",
+          "type": "text"
+        },
+        {
+          "name": "cat_name",
+          "type": "text"
+        },
+        {
+          "name": "cat_value",
+          "type": "text"
+        },
+        {
+          "name": "obj_name",
+          "type": "text"
+        },
+        {
+          "name": "obj_label",
+          "type": "text"
+        },
+        {
+          "name": "caption",
+          "type": "text"
+        },
+        {
+          "name": "lote_number",
+          "type": "text"
+        },
+        {
+          "name": "find_location",
+          "type": "text"
+        },
+        {
+          "name": "date_documented",
+          "type": "text"
+        },
+        {
+          "name": "obj_description",
+          "type": "text"
+        },
+        {
+          "name": "set_timestamp",
+          "type": "text"
+        },
+        {
+          "name": "informe_designation",
+          "type": "text"
+        }
+      ],
+      "outputType": "array"
+    },
+    "dview_images": {
+      "meta": [
+        {
+          "type": "text",
+          "name": "thumb"
+        },
+        {
+          "type": "number",
+          "name": "id_images"
+        },
+        {
+          "type": "text",
+          "name": "image_number"
+        },
+        {
+          "type": "string",
+          "name": "figure_caption"
+        },
+        {
+          "type": "number",
+          "name": "fk_records"
+        },
+        {
+          "type": "text",
+          "name": "filename"
+        }
+      ],
+      "outputType": "array"
+    },
+    "repeat1": {
+      "outputType": "boolean"
+    },
+    "rpt_images": {
+      "meta": [
+        {
+          "type": "text",
+          "name": "thumb"
+        },
+        {
+          "type": "number",
+          "name": "id_images"
+        },
+        {
+          "type": "text",
+          "name": "image_number"
+        },
+        {
+          "type": "string",
+          "name": "figure_caption"
+        },
+        {
+          "type": "number",
+          "name": "fk_records"
+        },
+        {
+          "type": "text",
+          "name": "filename"
+        }
+      ],
+      "outputType": "array"
+    }
+  }
+});
---
+++ views/partials/formatter.js
@@ -0,0 +1,3 @@
+dmx.Formatter('string', 'parseJSON', function(str) {
+return JSON.parse(str)
+});
